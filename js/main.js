0879a56a84accc4d59257283fc4fdae9dc147841
cole-allen <coleallen3333@gmail.com>
2021-09-30 15:06:36 -0700

    Basica functionality of random complete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var $recipeName = document.querySelector('.title h1');
 var $recipeSource = document.querySelector('.source h3');
 var $recipeImage = document.querySelector('.recipe-image img');
-var $recipeInstructions = document.querySelector('.recipe-instructions p');
+var $recipeIngredients = document.querySelector('.recipe-ingredients tbody');
+var $recipeInstructions = document.querySelector('.recipe-instructions');
 
 getRandomRecipe();
 
@@ -13,12 +14,35 @@ function getRandomRecipe() {
     $recipeName.textContent = recipeJSON.meals[0].strMeal;
     $recipeSource.textContent = recipeJSON.meals[0].strSource; // change to a link
     $recipeImage.setAttribute('src', recipeJSON.meals[0].strMealThumb);
-    $recipeInstructions.textContent = recipeJSON.meals[0].strInstructions; // Make instructions more readable
+    for (var j = 1; j < 21; j++) {
+      $recipeIngredients.appendChild(getIngredients(recipeJSON.meals[0]['strIngredient' + j], recipeJSON.meals[0]['strMeasure' + j]));
+    }
+    var instructions = recipeJSON.meals[0].strInstructions.split('\n');
+    for (var i = 0; i < instructions.length; i++) {
+      var $recipeStep = document.createElement('p');
+      $recipeStep.setAttribute('class', 'recipe-step');
+      $recipeStep.textContent = instructions[i];
+      $recipeInstructions.appendChild($recipeStep);
+    }
   });
   recipe.open('GET', 'https://www.themealdb.com/api/json/v1/1/random.php');
   recipe.send();
 }
 
-function getIngredients(recipe) {
+function getIngredients(recipeIngredient, recipeAmount) {
+  var $recipeRow = document.createElement('tr');
+  var $recipeIngredient = document.createElement('td');
+  var $recipeMeasure = document.createElement('td');
+
+  $recipeIngredient.setAttribute('class', 'ingredient');
+  $recipeMeasure.setAttribute('class', 'ingredient-amount');
+
+  $recipeIngredient.textContent = recipeIngredient;
+  $recipeMeasure.textContent = recipeAmount;
+
+  $recipeRow.appendChild($recipeIngredient);
+  $recipeRow.appendChild($recipeMeasure);
+
+  return $recipeRow;
 
 }
